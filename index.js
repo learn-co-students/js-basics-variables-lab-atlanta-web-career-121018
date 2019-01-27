d82a52f140593690935bad0b987c5e82cd1057ac
github username <github email address>
2019-01-26 23:13:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+// describe('companyName', function () {
+//   it('is set as Scuber', function () {
+//     expect(companyName).to.equal('Scuber');
+//     expect(function () { companyName = 'specialCompany' }).to.throw(TypeError);
+        const companyName = "Scuber";
+
+
+// describe('mostProfitableNeighborhood', function () {
+//   it('is declared as equal to Chelsea', function () {
+//     expect(mostProfitableNeighborhood).to.equal('Chelsea');
+//   it('does not raise error if the mostProfitableNeighborhood is changed', function () {
+//     expect(function () { mostProfitableNeighborhood = 'Upper West Side' }).to.not.throw(TypeError);
+        let mostProfitableNeighborhood = 'Chelsea'
+        // function namegoeshere() {
+        //     let mostProfitableNeighborhood = 'Upper West Side'
+        // }
+// describe('companyCeo', function () {
+//   it('is declared as equal to Susan Smith', function () {
+//     expect(companyCeo).to.equal('Susan Smith');
+//   it('does not raise error if the companyCeo is changed', function () {
+//     expect(function () { companyCeo = 'Lauren Hart' }).to.not.throw(TypeError);
+        let companyCeo = "Susan Smith"
